d1dc15105514d0cfb908a6740ce5a100a40bc7c5
pmorski <morski.przemek@gmail.com>
2019-12-20 14:20:01 +0100

    register with local eureka

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/PrzemyslawMorski/backing-fulfillment/service"
+
+	"github.com/hudl/fargo"
 )
 
 func main() {
@@ -12,8 +16,28 @@ func main() {
 		port = "3001"
 	}
 
-	// Ordinarily we'd use a CF environment here, but we don't need it for
-	// the fake data we're returning.
+	c := fargo.NewConn("http://localhost:18080/eureka/v2")
+
+	portInt, err := strconv.Atoi(port)
+	if err != nil {
+		fmt.Println("Port is not an int")
+		return
+	}
+
+	i := fargo.Instance{
+		HostName:         "backing-fulfillment",
+		Port:             portInt,
+		App:              "backing-fulfillment",
+		IPAddr:           "127.0.0.1",
+		VipAddress:       "127.0.0.1",
+		SecureVipAddress: "127.0.0.1",
+		DataCenterInfo:   fargo.DataCenterInfo{Name: fargo.MyOwn},
+		Status:           fargo.UP,
+	}
+
+	_ = c.RegisterInstance(&i)
+
+	// Ordinarily we'd use a CF environment here, but we don't need it for the fake data we're returning.
 	server := service.NewServer()
 	server.Run(":" + port)
 }
